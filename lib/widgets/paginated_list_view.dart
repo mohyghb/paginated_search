073a7e963ea6f238bc3b93e856d9c3ee79ee6767
Moh Yaghoub <moh10moh78@gmail.com>
2023-09-11 19:20:39 -0700

    Added PaginatedListView and PaginatedGridView widgets

---
+++ lib/widgets/paginated_list_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'abstract_paginated_view.dart';
+
+// Helper class for showing items of a paginated search in a list view
+class PaginatedListView<T, F> extends AbstractPaginatedView<T, F> {
+  final Axis scrollDirection;
+  final bool reverse;
+  final ScrollController? controller;
+  final bool? primary;
+  final ScrollPhysics? physics;
+  final bool shrinkWrap;
+  final double? cacheExtent;
+  final int? semanticChildCount;
+  final DragStartBehavior dragStartBehavior;
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+  final String? restorationId;
+  final Clip clipBehavior;
+
+  const PaginatedListView({
+    super.key,
+    required super.paginatedController,
+    required super.itemBuilder,
+    super.loadingBuilder,
+    super.errorBuilder,
+    super.emptyBuilder,
+    this.scrollDirection = Axis.vertical,
+    this.reverse = false,
+    this.controller,
+    this.primary,
+    this.physics,
+    this.shrinkWrap = false,
+    this.cacheExtent,
+    this.semanticChildCount,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+    this.restorationId,
+    this.clipBehavior = Clip.hardEdge,
+  });
+
+  @override
+  Widget whenData(BuildContext context, List<T> items) {
+    if (items.isEmpty && emptyBuilder != null) {
+      return emptyBuilder?.call(context) ?? const SizedBox();
+    }
+    return ListView.builder(
+      scrollDirection: scrollDirection,
+      reverse: reverse,
+      controller: controller,
+      primary: primary,
+      physics: physics,
+      shrinkWrap: shrinkWrap,
+      cacheExtent: cacheExtent,
+      semanticChildCount: semanticChildCount,
+      dragStartBehavior: dragStartBehavior,
+      keyboardDismissBehavior: keyboardDismissBehavior,
+      restorationId: restorationId,
+      clipBehavior: clipBehavior,
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return itemBuilder(items[index]);
+      },
+    );
+  }
+}
